1c7c362a2e69a3d40677d0ae030d345523103a1d
Viduttam <viduttam@gmail.com>
2021-03-18 00:08:32 -0400

    learning cin and cout

--- MyFirstApp/MyFirstApp.cpp
+++ MyFirstApp/MyFirstApp.cpp
@@ -8,6 +8,18 @@ int main()
 {
     cout << "Hello World!\n";
     cout << "I am Vengeance!"<< endl;
+
+    float annualSalary;
+    cout << "Please enter your annual salary" << endl;
+    cin >> annualSalary;
+
+    float monthlySalary = annualSalary / 12.0;
+    cout << "Your monthly salary is " << monthlySalary << endl;
+
+    cout << "In 10 years, you will earn " << annualSalary * 10.0;
+
+    char character = 'v';
+
     system("pause>0");
 }
 
